ffc26877139e3a31f05ba2934cecea58df881231
ben <koppe.development@gmail.com>
2026-05-04 13:12:14 -0400

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "CMSX autograder";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.rustc
+              pkgs.rustfmt
+              pkgs.rust-analyzer
+              pkgs.sqlite
+              pkgs.uv
+              pkgs.python314
+            ];
+          };
+        };
+    };
+}
