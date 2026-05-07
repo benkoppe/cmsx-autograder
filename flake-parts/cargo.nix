ada19be4eaa57219f1f2ccefb305caf219a90844
ben <koppe.development@gmail.com>
2026-05-07 03:15:20 -0400

    Working sqlx compile and tests in cargo.nix

--- flake-parts/cargo.nix
+++ flake-parts/cargo.nix
@@ -6,25 +6,42 @@
       craneLib = inputs.crane.mkLib pkgs;
       unfilteredRoot = ../.;
 
+      cargoFileset = craneLib.fileset.commonCargoSources unfilteredRoot;
+
+      workspaceFileset = lib.fileset.unions [
+        cargoFileset
+        (lib.fileset.maybeMissing ../migrations)
+        (lib.fileset.maybeMissing ../.sqlx)
+      ];
+
+      workspaceTestFileset = lib.fileset.unions [
+        workspaceFileset
+        (lib.fileset.maybeMissing ../python/sdk/src)
+      ];
+
       cargoSrc = lib.fileset.toSource {
         root = unfilteredRoot;
-        fileset = craneLib.fileset.commonCargoSources unfilteredRoot;
+        fileset = cargoFileset;
       };
 
       workspaceSrc = lib.fileset.toSource {
         root = unfilteredRoot;
-        fileset = lib.fileset.unions [
-          (craneLib.fileset.commonCargoSources unfilteredRoot)
-          (lib.fileset.maybeMissing ../migrations)
-          (lib.fileset.maybeMissing ../.sqlx)
-        ];
+        fileset = workspaceFileset;
+      };
+
+      workspaceTestSrc = lib.fileset.toSource {
+        root = unfilteredRoot;
+        fileset = workspaceTestFileset;
       };
 
       commonBuildArgs = {
         strictDeps = true;
 
+        env.SQLX_OFFLINE = "true";
+
         nativeBuildInputs = [
           pkgs.cmake
+          pkgs.git
           pkgs.pkg-config
           pkgs.postgresql
         ]
@@ -74,8 +91,32 @@
         rust-tests = craneLib.cargoNextest (
           commonBuildArgs
           // {
-            src = workspaceSrc;
+            src = workspaceTestSrc;
             inherit cargoArtifacts;
+            nativeBuildInputs = commonBuildArgs.nativeBuildInputs ++ [
+              pkgs.python314
+              pkgs.postgresql
+            ];
+
+            preCheck = ''
+              export PGDATA="$TMPDIR/postgres"
+              export PGHOST="$TMPDIR/postgres-socket"
+              export PGPORT="5432"
+              mkdir -p "$PGHOST"
+
+              initdb "$PGDATA" --auth=trust --no-locale
+              pg_ctl \
+                -D "$PGDATA" \
+                -o "-k $PGHOST -p $PGPORT" \
+                -l "$TMPDIR/postgres.log" \
+                start
+
+              export CMSX_DATABASE_URL="postgresql://localhost/cmsx?host=$PGHOST&port=$PGPORT"
+            '';
+            postCheck = ''
+              pg_ctl -D "$PGDATA" stop || true
+            '';
+
             partitions = 1;
             partitionType = "count";
             cargoNextestPartitionsExtraArgs = "--no-tests=pass";
